95c38853f2f4a86edb176c06cf99c2669b2ca36d
JackR42 <j.roumimper@gmail.com>
2022-11-17 16:08:20 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
 variable "prefix" {
   description = "The prefix to be used for all resources."
-  default = ["Project08-"]
 }
 
 variable "location" {
   description = "The Azure Region in which to create all resources."
-  default = ["west-europe"]
 }
