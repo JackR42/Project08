7a45672c98ae81bd7eed982b6df1ca55225df251
JackR42 <j.roumimper@gmail.com>
2022-11-18 00:09:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,10 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
+
+resource "azurerm_subnet" "example" {
+  name                 = "${var.prefix}-SN"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
