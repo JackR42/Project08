20e42182018a35e4ae4836d08100980ea4250552
JackR42 <j.roumimper@gmail.com>
2022-11-17 23:55:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,5 +12,16 @@ terraform {
 resource "azurerm_resource_group" "example" {
   name = "S1-${var.prefix}-RG"
   location = var.location
-#  location = "westeurope"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-VN"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
