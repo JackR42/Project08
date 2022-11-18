e4640fc6e10d1692486df7a842d8b04a4c0793ef
JackR42 <j.roumimper@gmail.com>
2022-11-18 01:29:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ resource "azurerm_virtual_machine" "example" {
     name              = "${var.prefix}-OSDisk"
     caching           = "ReadOnly"
     create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+    managed_disk_type = "Premium_SSD_LRS"
   }
 
   os_profile {
